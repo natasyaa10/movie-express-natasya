3c73c88ea041125105f8eb0d37123617b42814f1
natasyaa10 <natasyapriscill10@gmail.com>
2025-11-15 18:35:55 +0700

    Pertemuan tgl 15 nov 2025

--- movie-express/app.js
+++ movie-express/app.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import api from "./routes/api.js";
 import database from "./config/database.js";
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors())
 app.use(express.json());
+
 app.use("/api", api);
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({
-//         message : "OK"
-//     })
-// })
+app.get('/', (req, res) => {
+     res.status(200).json({
+         message : "OK"
+     })
+ })
 
 app.listen(3000, () => {
     database();
